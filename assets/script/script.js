783c6c0d029b8bc6dbf1863fcdccd7af93b11b0c
github <calbest31@outlook.com>
2021-04-10 21:10:17 -0400

    todays date is working

---
+++ assets/script/script.js
@@ -0,0 +1,13 @@
+var timeArray = [
+    "9:00AM",
+    "10:00AM",
+    "11:00AM",
+    "12:00PM",
+    "1:00PM",
+    "2:00PM",
+    "3:00PM",
+    "4:00PM",
+    "5:00PM",
+]
+
+document.getElementById('currentDay').textContent = moment().format('dddd, MMMM Do');
